dc65699870b2640351d7aa99c78cd4f3bd16dc60
Nnam-Danirl <nnamarinze8@gmail.com>
2025-02-25 10:23:50 +0100

    update parenthesis

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ int main(void)
 	/* your code goes there
 	scanf("%d\n", &n);*/
 	if(n > 0)
-          {
-           printf("%d is positive\n", n);
-          }
+        {
+                printf("%d is positive\n", n);
+        }
         else if(n==0)
         {
                 printf("%d is zero\n", n);
